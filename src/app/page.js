fceb99d52dde2664ce47b7ae91c71805ee1996e0
MG4ACA <mg4.aca@gmail.com>
2025-10-20 02:26:06 +0530

    implementation done

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,12 @@
 import About from '@/components/About';
 import BrandLogos from '@/components/BrandLogos';
-import Contact from '@/components/Contact';
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 import Hero from '@/components/Hero';
 import OurAdvantage from '@/components/OurAdvantage';
-import WhyChooseUs from '@/components/WhyChooseUs';
 import Services from '@/components/Services';
 import TeamSection from '@/components/TeamSection';
+import WhyChooseUs from '@/components/WhyChooseUs';
 
 export default function Home() {
   return (
@@ -16,11 +15,10 @@ export default function Home() {
       <Hero />
       <About />
       <Services />
-  <OurAdvantage />
-  <WhyChooseUs />
+      <OurAdvantage />
       <BrandLogos />
       <TeamSection />
-      <Contact />
+      <WhyChooseUs />
       <Footer />
     </main>
   );
