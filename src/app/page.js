671819bbcd138d04b094bd814a430e37747c2080
MG4ACA <mg4.aca@gmail.com>
2025-10-20 01:33:07 +0530

    image carosal section added

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
 import About from '@/components/About';
+import BrandLogos from '@/components/BrandLogos';
 import Contact from '@/components/Contact';
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 import Hero from '@/components/Hero';
 import OurAdvantage from '@/components/OurAdvantage';
-import BrandLogos from '@/components/BrandLogos';
 import Services from '@/components/Services';
+import TeamSection from '@/components/TeamSection';
 
 export default function Home() {
   return (
@@ -14,8 +15,9 @@ export default function Home() {
       <Hero />
       <About />
       <Services />
-  <OurAdvantage />
-  <BrandLogos />
+      <OurAdvantage />
+      <BrandLogos />
+      <TeamSection />
       <Contact />
       <Footer />
     </main>
